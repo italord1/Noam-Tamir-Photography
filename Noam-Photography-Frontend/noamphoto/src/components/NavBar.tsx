435dc6361d1eb1132b8a4c6c70b3c40a96bb58d6
Itai Galipoliti <itai.galipo@gmail.com>
2025-10-12 19:41:45 +0300

    hamburger icon

--- Noam-Photography-Frontend/noamphoto/src/components/NavBar.tsx
+++ Noam-Photography-Frontend/noamphoto/src/components/NavBar.tsx
@@ -1,27 +1,28 @@
 import { Link } from "react-router-dom";
 import "../Styles/navbar.css";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 
 function Navbar() {
 
-  
+  const [isOpen, setIsOpen] = useState(false);
+
+
   return (
-    <nav className="navbar">
+     <nav className="navbar">
       <div className="logo">Noam Tamir Photography</div>
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/categories">Categories</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li>
-          <a
-            href="https://instagram.com/noamtamirphoto"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src="/instagram.svg" alt="Instagram" className="insta-icon" />
-          </a>
-        </li>
+
+      {/* Icon */}
+      <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+       {isOpen ? <FaTimes size={26} color="#f5f5f5" /> : <FaBars size={26} color="#f5f5f5" />}
+      </div>
+
+      <ul className={`nav-links ${isOpen ? "active" : ""}`}>
+        <li><Link to="/" onClick={() => setIsOpen(false)}>Home</Link></li>
+        <li><Link to="/categories" onClick={() => setIsOpen(false)}>Categories</Link></li>
+        <li><Link to="/about" onClick={() => setIsOpen(false)}>About</Link></li>
+        <li><Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link></li>
       </ul>
     </nav>
   );
